9621804cc7878b9356f2f6a09b15219a6a24f047
kushaal-rana <kurana@cs.stonybrook.edu>
2024-07-11 08:38:22 +0530

    utils code:API response and error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 // dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    //before app.listen listen for errors
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB CONNECTION FAILED !!!", error);
+  });
 
 /*
 const app = express();
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// How is it working
+// step1: const asyncHandler = ()=>{}
+// Step2: const asyncHandler = (func)=>{()=>{}} //just remove the curly brackets
+// Step2: const asyncHandler = (func)=> async()=>{}
+
+//Accepting function and passing it as argument to another function and executing it
+// const asyncHandler = (fn) => async (req, res, next)=>{
+//     try{
+//         await fn(req,res,next)
+//     }
+//     catch(error){
+//         res.status(error.code || error.message || 500).json({
+//             success: false,
+//             message: error.message || "Server Error"
+//         })
+//     }
+// } //Now this is a wrapper for a function which requres try catch checking, just pass the function as an argument
