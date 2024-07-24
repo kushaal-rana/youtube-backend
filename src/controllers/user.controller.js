c86a683438ae51ba69fdbfbcb94d3a02a1f5263d
kushaal-rana <kurana@cs.stonybrook.edu>
2024-07-24 18:38:55 +0530

    added getwatchhistory method & related pipelines,

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -391,6 +391,57 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        //_id: req.user?._id, //yaha pe mongoose kaam nahi karta aggregation pipeline ka jitna code hai wo directly jata  hai so we need to convert to mongoose object ID.
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res.status(
+    200,
+    user[0].watchHistory,
+    "Watch history fetched successfully."
+  );
+});
+
 export {
   registerUser,
   loginUser,
@@ -402,4 +453,5 @@ export {
   updateUserAvatarImage,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory,
 };
